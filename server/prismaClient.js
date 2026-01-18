2de06423631364e635c0ed541cbf2cc8cf1b403d
Noor <noorahmedd2288@gmail.com>
2026-01-18 13:39:53 +0200

    API with fuzzy search

---
+++ server/prismaClient.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const { PrismaClient } = require("@prisma/client");
+const { Pool } = require("pg");
+const { PrismaPg } = require("@prisma/adapter-pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg(pool),
+});
+
+module.exports = { prisma };
