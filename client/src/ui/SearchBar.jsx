ad7a02b8b18a38d53d7427314c904faf62342b8a
Noor <noorahmedd2288@gmail.com>
2026-01-20 13:47:37 +0200

    UI changes

--- client/src/ui/SearchBar.jsx
+++ client/src/ui/SearchBar.jsx
@@ -2,7 +2,7 @@ export default function SearchBar({ value, onChange }) {
   return (
     <input
       className="form-control"
-      style={{ width: 360, borderRadius: 12 }}
+      style={{ width: 360 }}
       placeholder="Search games..."
       value={value}
       onChange={(e) => onChange(e.target.value)}
